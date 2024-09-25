bea793c81c5300fb64e358f33daffb249bb3ed50
Jatin Mittal <jatinmittal164@gmail.com>
2024-09-25 20:06:27 +0530

    Configuration Update

---
+++ src/main/java/com/Feedy/configuration/JstConstant.java
@@ -0,0 +1,11 @@
+/**
+ * Author: Jatin Mittal
+ * Date: 25-09-2024
+ */
+
+package com.Feedy.configuration;
+
+public class JstConstant {
+    public static final String SECRET_KEY = "asfhklashfakgfalkdfgolfsakljfbsdkjfbso";
+    public static final String JWT_HEADER = "Authorization";
+}
---
+++ src/main/java/com/Feedy/configuration/JwtTokenValidator.java
@@ -0,0 +1,24 @@
+/**
+ * Author: Jatin Mittal
+ * Date: 25-09-2024
+ */
+
+package com.Feedy.configuration;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
