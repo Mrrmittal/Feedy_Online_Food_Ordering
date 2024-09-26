2076353364a5ff8293323204efcdb6ef0e5523b3
Jatin Mittal <jatinmittal164@gmail.com>
2024-09-26 19:08:18 +0530

    Jwt and AppConfiguration Update

--- src/main/java/com/Feedy/configuration/AppConfiguration.java
+++ src/main/java/com/Feedy/configuration/AppConfiguration.java
@@ -35,7 +35,8 @@ public class AppConfiguration {
                 ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 .csrf(csrf->csrf.disable())
                 .cors(cors->cors.configurationSource(corsConfigurationSource()));
-        return null;
+
+        return httpSecurity.build();
     }
 
 
